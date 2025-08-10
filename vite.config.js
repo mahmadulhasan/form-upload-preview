4ef4bdf7f9b355c85e2a813efecf8fbe36938dc6
MAHMADUL HASAN <mahmadulhasan623@gmail.com>
2025-08-10 23:51:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/form-upload-preview/' 
-})
+export default {
+  build: {
+    outDir: 'build'
+  }
+}
